803c210ee0ebec12527bd4e86d7766e3b6a503cd
Michael Amadheo <56286891+michmadheo@users.noreply.github.com>
2026-05-18 22:55:55 +0700

    ver:2.0.0 msg:Fix generate_env.dart commands

--- .arch/generate_env.dart
+++ .arch/generate_env.dart
@@ -3,11 +3,7 @@ import 'dart:io';
 Future<void> main() async {
   await Process.run('dart', ['run', 'build_runner', 'clean'], runInShell: true);
 
-  await Process.run('dart', [
-    'run',
-    'change_app_package_name:main',
-    'build',
-  ], runInShell: true);
+await Process.run('dart', ['run', 'build_runner', 'build'], runInShell: true);
 
   print('✅ Environment generated successfully');
 }
